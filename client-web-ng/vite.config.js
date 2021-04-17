366d0119bcef8f950382543b0f75e1830bd01fdb
WangLei <nihiue@gmail.com>
2021-04-17 18:32:51 +0800

    add brotliCompress

--- client-web-ng/vite.config.js
+++ client-web-ng/vite.config.js
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import viteCompression from 'vite-plugin-compression';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), viteCompression({
+    algorithm: 'brotliCompress',
+    ext: '.br'
+  })],
   base: '',
   server: {
     port: '8000',
