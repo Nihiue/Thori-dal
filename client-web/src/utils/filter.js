37c8f6ebf943bcd331681b521f23a2ef9cf4635f
WangLei <nihiue@gmail.com>
2021-06-16 16:52:12 +0800

    remove client-web

--- client-web/src/utils/filter.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue';
-import moment from 'moment';
-
-Vue.filter('datetime', function (value, fmt) {
-  if (!value) {
-    return 'N/A';
-  }
-  return moment(value).format(fmt || 'YYYY-MM-DD HH:mm');
-});
